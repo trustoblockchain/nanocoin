4e40eb62c7397bdcc160e6ef6aa181e065e3af09
Alex Vzorov <alex.vzorov@gmail.com>
2017-09-12 22:26:29 +0900

    Add logging variants of `print` and `putText`

--- src/Logger.hs
+++ src/Logger.hs
@@ -4,9 +4,12 @@ module Logger (
   Output(..),
   Logger(..),
   create,
+  print,
+  putText,
   MonadLogger(..)
 ) where
 
+import Protolude hiding (print, putText, ask)
 import System.Logger.Class
 import qualified System.Logger as Logger
 import Control.Monad.IO.Class
@@ -16,3 +19,9 @@ instance MonadIO m => MonadLogger (ReaderT Logger m) where
   log lvl f = do
     logger <- ask
     Logger.log logger lvl f
+
+print :: (MonadLogger m, Show a) => a -> m ()
+print = info . msg .(show :: Show a => a -> Text)
+
+putText :: MonadLogger m => Text -> m ()
+putText = info . msg
